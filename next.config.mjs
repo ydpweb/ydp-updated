835e1440e8e83a6082a57598913732966cca4fc2
ydpweb <ydpweb2021@gmail.com>
2025-03-08 21:31:12 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // Fix images not loading
+  },
+  trailingSlash: true, // Ensure correct linking
 };
 
 export default nextConfig;
